bb8f4f6f25f4ffb4fda9ca4cda35edfefa96e96e
Dmitriy <shkredovdmitriy@gmail.com>
2022-11-21 17:41:15 +0300

    feat: add custom router

--- src/app/_router.js
+++ src/app/_router.js
@@ -1,13 +1,17 @@
 import { store } from './_store';
 
-const menuStart = document.querySelector('.header__nav-start-link');
-const menuGame = document.querySelector('.header__nav-game-link');
-const menuResults = document.querySelector('.header__nav-res-link');
+const menuList = document.querySelectorAll('.header__nav-link');
+const menuStart = document.querySelector('#headerStartLink');
+const menuGame = document.querySelector('#headerGameLink');
+const menuResults = document.querySelector('#headerResultsLink');
+const menuGallery = document.querySelector('#headerGalleryLink');
 
+const sections = document.querySelectorAll('.section');
 const preloader = document.querySelector('.preloader');
 const pageStart = document.querySelector('.start');
 const pageGame = document.querySelector('.game');
 const pageResults = document.querySelector('.results');
+const pageGallery = document.querySelector('.gallery');
 
 const preloaderShow = () => {
   return new Promise((resp, rej) => {
@@ -25,22 +29,80 @@ const preloaderHide = () => {
   });
 };
 
-const showGame = () => {
+const allHide = () => {
   return new Promise((resp, rej) => {
     setTimeout(() => {
-      pageStart.classList.remove('show');
+      menuList.forEach((el) => el.classList.remove('active'));
+      sections.forEach((el) => el.classList.remove('show'));
+      resp();
+    }, 100);
+  });
+};
+
+const startShow = () => {
+  return new Promise((resp, rej) => {
+    setTimeout(() => {
+      menuStart.classList.add('active');
+      pageStart.classList.add('show');
+      resp();
+    }, 100);
+  });
+};
+
+const gameShow = () => {
+  return new Promise((resp, rej) => {
+    setTimeout(() => {
+      menuGame.classList.add('active');
       pageGame.classList.add('show');
       resp();
-    }, 500);
+    }, 100);
+  });
+};
+
+const resultsShow = () => {
+  return new Promise((resp, rej) => {
+    setTimeout(() => {
+      menuResults.classList.add('active');
+      pageResults.classList.add('show');
+      resp();
+    }, 100);
+  });
+};
+
+const galleryShow = () => {
+  return new Promise((resp, rej) => {
+    setTimeout(() => {
+      menuGallery.classList.add('active');
+      pageGallery.classList.add('show');
+      resp();
+    }, 100);
   });
 };
 
 export const pageStartShow = async () => {
   await preloaderShow();
-  await showGame();
+  await allHide();
+  await startShow();
   await preloaderHide();
-  menuStart.classList.remove('active');
-  menuGame.classList.add('active');
 };
 
-export const router = () => {};
+export const pageGameShow = async () => {
+  await preloaderShow();
+  await allHide();
+  await gameShow();
+  await preloaderHide();
+};
+
+export const pageResultsShow = async () => {
+  await preloaderShow();
+  await allHide();
+  await resultsShow();
+  await preloaderHide();
+};
+
+export const pageGalleryShow = async () => {
+  await preloaderShow();
+  await allHide();
+  await galleryShow();
+  await preloaderHide();
+};
